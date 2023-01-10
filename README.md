72ef4a14d11949f54f78401de28d96dc45b79ad9
826300474 <826300474@qq.com>
2023-01-10 13:58:05 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-快速创建react组件所需文件
+# 快速创建react组件
+
+## 组件命必须写在开头，之后可以输入参数（ts,less,memo,simple）,参数间用”,“隔开。
+
 ![image](https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/50b046432d32477cbc90c767d6b6a90c~tplv-k3u1fbpfcp-watermark.image)
+
+
+
+
--- extension.js
+++ extension.js
@@ -4,32 +4,33 @@ const fs = require('fs');
 
 const getFileContext = (params) => {
 
-	const [name,...restParams] = params;
+	const [name, ...restParams] = params;
 
 	const hasTs = restParams.includes('ts');
 	const hasLess = restParams.includes('less');
+	const hasMemo = restParams.includes('memo');
 
 	const template = [
-`import React from '@alipay/bigfish/react';`,
-`${ hasTs ? `\nimport type { FC } from '@alipay/bigfish/react';` : `` }`,
-`${ hasLess ? `\nimport styles from './index.less';` : `` }`,
-`${ hasTs ? `\n
+		`import React from '@alipay/bigfish/react';`,
+		`${hasTs ? `\nimport type { FC } from '@alipay/bigfish/react';` : ``}`,
+		`${hasLess ? `\nimport styles from './index.less';` : ``}`,
+		`${hasTs ? `\n
 interface Props {
 
-};` : `` }`,
-`\n
-const ${name}${ hasTs ? `: FC<Props>` : `` } = () => {
+};` : ``}`,
+		`\n
+const ${name}${hasTs ? `: FC<Props>` : ``} = () => {
 	return <></>
 };`,
-`\n
-export default ${name};`
+		`\n
+export default ${hasMemo ? `React.memo(${name})` : name};`
 	]
 
-	return `${template.filter(d=>d).join('')}`;
+	return `${template.filter(d => d).join('')}`;
 }
 
 function activate(context) {
-	
+
 	let disposable = vscode.commands.registerCommand('react-component.creat', function (url) {
 
 		let uri = url.fsPath;
@@ -37,11 +38,25 @@ function activate(context) {
 		vscode.window.showInputBox({ placeHolder: '请输入组件名称' }).then((inputValue) => {
 			if (inputValue) {
 				const params = inputValue.split(',');
-				const [componentName,...restParams] = params;
-				
+				const [componentName, ...restParams] = params;
+				const hasLess = restParams.includes('less');
+				const hasSimple = restParams.includes('simple');
+
+				if (hasSimple) {
+					const indexUrl = `${uri}/${componentName}.tsx`;
+					fs.writeFile(indexUrl, getFileContext(params), function (err) {
+						if (err) {
+							vscode.window.showErrorMessage(err.message);
+						} else {
+							vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.file(indexUrl))
+						}
+					});
+					return;
+				}
 				fs.mkdir(`${uri}/${componentName}`, function (err) {
 					if (!err) {
 						const indexUrl = `${uri}/${componentName}/index.tsx`;
+						// console.log('indexUrl',indexUrl);
 						fs.writeFile(indexUrl, getFileContext(params), function (err) {
 							if (err) {
 								vscode.window.showErrorMessage(err.message);
@@ -50,7 +65,7 @@ function activate(context) {
 							}
 						});
 
-						if( restParams.includes('less') ){
+						if (hasLess) {
 							const lessUrl = `${uri}/${componentName}/index.less`;
 							fs.writeFile(lessUrl, ``, function (err) {
 								if (err) {
@@ -58,13 +73,13 @@ function activate(context) {
 								}
 							});
 						}
-		
+
 					} else {
 						vscode.window.showErrorMessage(err.message);
 					}
 				});
 
-				
+
 			}
 		})
 	});
